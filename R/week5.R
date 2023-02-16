9ac9e97917ac7be1e02b2c53725f17cb8a23e275
Vivien Lee <vivienlvv@gmail.com>
2023-02-16 11:05:05 -0600

    Data import complete

--- R/week5.R
+++ R/week5.R
@@ -2,3 +2,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+
+# Data Import 
+Adata_tbl = read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum","stimver","datadate","qs"), show_col_types = FALSE)
+Anotes_tbl = read_csv("../data/Anotes.csv", show_col_types = FALSE)
+Bdata_tbl = read_delim("../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q",1:10)), show_col_types = FALSE)
+Bnotes_tbl = read_delim("../data/Bnotes.txt", delim = "\t", show_col_types = FALSE)
