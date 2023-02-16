1acac76f6a9869cabf870e9304ebb771e5b6f8b8
Vivien Lee <vivienlvv@gmail.com>
2023-02-16 11:23:08 -0600

    Data cleaning complete

--- R/week5.R
+++ R/week5.R
@@ -8,3 +8,24 @@ Adata_tbl = read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("
 Anotes_tbl = read_csv("../data/Anotes.csv", show_col_types = FALSE)
 Bdata_tbl = read_delim("../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q",1:10)), show_col_types = FALSE)
 Bnotes_tbl = read_delim("../data/Bnotes.txt", delim = "\t", show_col_types = FALSE)
+
+# Data Cleaning 
+Aclean_tbl = Adata_tbl %>% 
+  separate(col = qs, into = paste0("q",1:5), sep = " - ") %>% 
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %T")) %>% 
+  mutate(across(q1:q5, as.integer)) %>% 
+  left_join(Anotes_tbl, by = "parnum") %>% 
+  filter(is.na(notes))
+Bdata_tbl %>% 
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %T")) %>% 
+  mutate(across(q1:q10, as.integer)) %>% 
+  left_join(Bnotes_tbl, by = "parnum") %>% 
+  filter(is.na(notes)) %>% 
+  select(-notes) %>% 
+  bind_rows(Aclean_tbl, .id = "lab") # Double check
+  
+  
+
+  
+  
+  
